d32af0f319ec3345a03fdb01e8aeb712b9b8e0b1
reezyee <rezasapitra514@gmail.com>
2025-04-18 01:37:40 +0700

    first commit

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,4 +3,21 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: false },
   modules: ["@nuxtjs/tailwindcss"],
+  app: {
+    head: {
+      title: "Jasa Raharja Kota Tasikmalaya",
+      meta: [
+        { charset: "utf-8" },
+        { name: "viewport", content: "width=device-width, initial-scale=1" },
+        {
+          name: "description",
+          content: "Website statis Jasa Raharja Tasikmalaya Kota",
+        },
+      ],
+      link: [{ rel: "icon", type: "image/x-icon", href: "logo.ico" }],
+    },
+  },
+  nitro: {
+    preset: "vercel", // Use the Vercel preset
+  },
 })
